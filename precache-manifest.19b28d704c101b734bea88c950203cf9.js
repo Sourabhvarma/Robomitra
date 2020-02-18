70be6d25ac70e4dd61a994846de5d2e5b316c4e5
Sourabhvarma <sourabhvarma25@gmail.com>
2020-02-18 17:32:01 +0530

    Updates

---
+++ precache-manifest.19b28d704c101b734bea88c950203cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a772753bf139d4801951d1e617e5b04c",
+    "url": "/Robomitra/index.html"
+  },
+  {
+    "revision": "d22b24f406a99f4edd1e",
+    "url": "/Robomitra/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e8ed5ea52d534e14c76e",
+    "url": "/Robomitra/static/css/main.095546f3.chunk.css"
+  },
+  {
+    "revision": "d22b24f406a99f4edd1e",
+    "url": "/Robomitra/static/js/2.df293792.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Robomitra/static/js/2.df293792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ed5ea52d534e14c76e",
+    "url": "/Robomitra/static/js/main.c645124d.chunk.js"
+  },
+  {
+    "revision": "095be543d47bda470041",
+    "url": "/Robomitra/static/js/runtime-main.0f26f8d7.js"
+  }
+]);
